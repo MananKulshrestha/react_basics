e961afdd2380077088db357c827bff9bfef30ca6
Manan Kulshrestha <manan.kulshrestha05@gmail.com>
2026-04-13 00:17:01 +0530

    minor changes

--- src/Food.jsx
+++ src/Food.jsx
@@ -2,7 +2,7 @@
 function Food(){
     const food = "Pizza";
     const price = 10;
-    return (<div classname = "c1">
+    return (<div className = "c1">
         <h4>Food: {food}</h4>
         <p>Price: ${price}</p>
     </div>);
